6756c0d278c4f1542b3671b7cd9448178271e664
Yuval Navon <yuvalnavon8@gmail.com>
2022-01-11 23:28:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/internalfilesex/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.internalfilesex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    FileOutputStream fos;
+    FileInputStream fis;
+    OutputStreamWriter osw;
+    InputStreamReader isr;
+    BufferedWriter bw;
+    BufferedReader br;
+    StringBuffer sb;
+    String text, saved;
+    EditText et;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.editTextTextPersonName);
+        tv = findViewById(R.id.textView2);
+        text = "";
+        saved = "";
+        try {
+            fos = openFileOutput("test.txt",MODE_PRIVATE);
+            osw = new OutputStreamWriter(fos);
+            bw = new BufferedWriter(osw);
+            bw.write("hello");
+            bw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void Save(View view)
+    {
+        text = et.getText().toString();
+        try {
+            bw.write(saved + text);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public void Reset(View view)
+    {
+        try {
+            bw.write("");
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        tv.setText("");
+        et.setText("");
+        text = "";
+        saved = "";
+    }
+
+    public void Exit(View view)
+    {
+
+    }
+
+
+
+    public boolean onCreateOptionsMenu (Menu menu)
+    /**
+     *  Creates a general OptionsMenu which allows the user to choose which
+     *  allows the user to choose which screen they want to use, the main
+     *  screen or the credits screen.
+     *
+     *
+     * @param    Menu menu -required for the function to inflate the menu layout into
+     *           the Menu object which it creates.
+     * @return	true
+     */
+    {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    /**
+     * Decides which option the user chose and sends them to the other screen
+     * if they picked it.
+     *
+     *
+     * @param    MenuItem item - the option which the user chose.
+     * @return	true
+     */
+    {
+        int id = item.getItemId();
+        if (id == R.id.Credits)
+        {
+            Intent si = new Intent(this, Credits.class);
+            startActivity(si);
+        }
+        return true;
+
+
+    }
+
+}
