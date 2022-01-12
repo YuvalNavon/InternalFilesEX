ee8c79816f58bdadd8c3d62f6dd830a85581598f
Yuval Navon <yuvalnavon8@gmail.com>
2022-01-12 18:25:53 +0200

    3

--- app/src/main/java/com/example/internalfilesex/Credits.java
+++ app/src/main/java/com/example/internalfilesex/Credits.java
@@ -1,8 +1,8 @@
 /**
  * @author		Yuval Navon <yuvalnavon8@gmail.com>
  * @version	1.0
- * @since		11/1/2022
- * Just a credits screen with an OptionsMenu to go back to the main activity
+ * @since		12/1/2022
+ * Just a credits screen with an OptionsMenu to go back to the main activity.
  */
 package com.example.internalfilesex;
 
@@ -14,13 +14,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Credits extends AppCompatActivity {
-    /**
-     * The class creates the activity and the OptionsMenu, nothin' else.
-     *
-     *
-     * *@param	None
-     * @return	None
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +22,6 @@ public class Credits extends AppCompatActivity {
 
     }
 
-    public boolean onCreateOptionsMenu (Menu menu)
     /**
      * Creates a general OptionsMenu which allows the user to choose which
      * allows the user to choose which screen they want to use, the main
@@ -40,6 +32,7 @@ public class Credits extends AppCompatActivity {
      *                the Menu object which it creates.
      * @return	true
      */
+    public boolean onCreateOptionsMenu (Menu menu)
     {
 
         getMenuInflater().inflate(R.menu.main, menu);
@@ -47,7 +40,6 @@ public class Credits extends AppCompatActivity {
         return true;
     }
 
-    public boolean onOptionsItemSelected(MenuItem item)
     /**
      * The function decides which option the user chose, and acts accordingly.
      * If they chose the Menu option, the function will go back to the first/main activity.
@@ -57,6 +49,7 @@ public class Credits extends AppCompatActivity {
      * @param    MenuItem item - the option which the user chose.
      * @return	true
      */
+    public boolean onOptionsItemSelected(MenuItem item)
     {
         int id = item.getItemId();
         if (id == R.id.Main)
--- app/src/main/java/com/example/internalfilesex/MainActivity.java
+++ app/src/main/java/com/example/internalfilesex/MainActivity.java
@@ -1,14 +1,23 @@
+/**
+ * @author		Yuval Navon <yuvalnavon8@gmail.com>
+ * @version	1.0
+ * @since		12/1/2022
+ * The program lets the user input text and write it in the phone's internal storage in a txt file.
+ * It also lets him reset the file, exit and read the file upon starting the app again.
+ */
 package com.example.internalfilesex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -27,9 +36,11 @@ public class MainActivity extends AppCompatActivity {
     BufferedWriter bw;
     BufferedReader br;
     StringBuffer sb;
-    String text, saved;
+    String text;
     EditText et;
     TextView tv;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,33 +49,22 @@ public class MainActivity extends AppCompatActivity {
 
         et = findViewById(R.id.editTextTextPersonName);
         tv = findViewById(R.id.textView2);
+        tv.setText("");
         text = "";
-        saved = "";
-        try {
-            fos = openFileOutput("test.txt",MODE_PRIVATE);
-            osw = new OutputStreamWriter(fos);
-            bw = new BufferedWriter(osw);
-            bw.write("hello");
-            bw.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
 
 
     }
 
-    public void Save(View view)
-    {
-        text = et.getText().toString();
-        try {
-            bw.write(saved + text);
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    /**
+     * Reads the test.txt file, saves its content to the String text and puts
+     * it in the textView tv.
+     * @param   None
+     * @return	None
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
         try {
             fis= openFileInput("test.txt");
             isr = new InputStreamReader(fis);
@@ -75,9 +75,10 @@ public class MainActivity extends AppCompatActivity {
                 sb.append(line+'\n');
                 line = br.readLine();
             }
-            saved =sb.toString();
-            tv.setText(saved);
+            text=sb.toString();
             br.close();
+            tv.setText(text);
+
 
 
         }
@@ -88,33 +89,75 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
-
     }
 
-    public void Reset(View view)
+
+    /**
+     * Adds the user's input into the test.txt file.
+     * @param   None
+     * @return	None
+     */
+    public void Saver()
     {
         try {
-            bw.write("");
+            fos = openFileOutput("test.txt",MODE_PRIVATE);
+            osw = new OutputStreamWriter(fos);
+            bw = new BufferedWriter(osw);
+            bw.write(text);
             bw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        tv.setText("");
-        et.setText("");
-        text = "";
-        saved = "";
     }
 
+    /**
+     * Called when the "SAVE" Button is pressed.
+     * Uses the Saver function and sets the entire text that the user inputted in the textView tv.
+     * @param   None
+     * @return	None
+     */
+    public void Save(View view)
+    {
+        text += et.getText().toString();
+        Saver();
+        tv.setText(text);
+
+    }
+
+    /**
+     * Called when the "RESET" Button is pressed.
+     * Resets the textView tv, String text and uses the Saver function to reset the test.txt file.
+     * @param   None
+     * @return	None
+     */
+    public void Reset(View view)
+    {
+        text = "";
+        Saver();
+        tv.setText("");
+        et.setText("");
+
+
+    }
+
+    /**
+     * Called when the "EXIT" Button is pressed.
+     * Uses the Saver function and exits the app.
+     * @param   None
+     * @return	None
+     */
     public void Exit(View view)
     {
 
+        text+=et.getText().toString();
+        Saver();
+        finish();
     }
 
 
 
-    public boolean onCreateOptionsMenu (Menu menu)
     /**
      *  Creates a general OptionsMenu which allows the user to choose which
      *  allows the user to choose which screen they want to use, the main
@@ -125,6 +168,8 @@ public class MainActivity extends AppCompatActivity {
      *           the Menu object which it creates.
      * @return	true
      */
+    public boolean onCreateOptionsMenu (Menu menu)
+
     {
 
         getMenuInflater().inflate(R.menu.main, menu);
@@ -132,7 +177,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public boolean onOptionsItemSelected(MenuItem item)
     /**
      * Decides which option the user chose and sends them to the other screen
      * if they picked it.
@@ -141,6 +185,8 @@ public class MainActivity extends AppCompatActivity {
      * @param    MenuItem item - the option which the user chose.
      * @return	true
      */
+    public boolean onOptionsItemSelected(MenuItem item)
+
     {
         int id = item.getItemId();
         if (id == R.id.Credits)
